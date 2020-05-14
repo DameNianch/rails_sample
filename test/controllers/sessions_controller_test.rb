43ea0b9afb5e35fcfb78a7fd9269791fc5cd2530
kento-kobayashi <medaca.pro@gmail.com>
2020-05-14 18:35:17 +0900

    ああああ

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get login_path
+    assert_response :success
+  end
+  
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
